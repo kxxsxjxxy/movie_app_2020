480472a97d4653b428fa8be3f6d2e62a01d39b48
kxxsxjxxy <kxxsxjxxy@naver.com>
2020-09-23 01:48:18 +0900

    Edit README.MD,App.js

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ React JS Fundamentals Course 2020
 3. 컴퍼넌트 이름의 첫자는 반드시 대문자로 해야한다.
 4. 컴퍼넌트에 데이터를 전달 할 때는 props를 사용한다.
 6. 리액트는 다수의 컴포넌트로 이루어지거나, 최종적으로는 하나의 컴포넌트만 사용한다.
+
+## 9월 11일
+1. Dothome에 indexfile 수정하기
+2. readme.md 수정 후 githun에 push하기 
--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
+
+function Food({fav}){
+return <h1>I like {fav} </h1>;
+}
 function App() {
-  return  <div className="App" />;
+  // return  <div className="App"/>;
+
+  return  (
+  <div> 
+    <h1>hello</h1>
+    <Food fav="김치" />
+    <Food fav="떡볶이" />
+    <Food fav="치킨" />
+    <Food fav="피자" />
+  </div>
+  );
 }
 
 export default App;
